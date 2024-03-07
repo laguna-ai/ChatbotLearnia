6d29f1c204550d11200b4471f09bb35794377258
diemtca <diemtca@gmail.com>
2024-03-07 18:06:26 -0500

    agregar argumento para habilitar deserialización de archivo .pkl del index

--- Learnia_whatsapp/index_query.py
+++ Learnia_whatsapp/index_query.py
@@ -16,7 +16,7 @@ llm_chain = LLMChain(llm=llm, prompt=prompt)
 # define embedding
 embeddings = OpenAIEmbeddings(openai_api_key=key)
 # load vector database
-db = FAISS.load_local("Learnia_whatsapp/index", embeddings)
+db = FAISS.load_local("Learnia_whatsapp/index", embeddings,allow_dangerous_deserialization=True)
 # define retriever
 retriever = db.as_retriever(
     search_type="similarity_score_threshold",
