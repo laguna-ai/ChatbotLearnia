d50dc18607182cadd9fddad9a01f57b6aa687cd2
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2024-03-12 18:19:19 -0500

    update index_query.py

--- Learnia_whatsapp/index_query.py
+++ Learnia_whatsapp/index_query.py
@@ -1,11 +1,9 @@
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain_community.vectorstores.faiss import FAISS
 from langchain.chains import StuffDocumentsChain, LLMChain
 import os
 
-###############################################################################################
 llm_name = "gpt-3.5-turbo"
 key = os.environ["OPENAI_API_KEY"]
 template = "Summarize and Order this content: {context}"
@@ -16,19 +14,22 @@ llm_chain = LLMChain(llm=llm, prompt=prompt)
 # define embedding
 embeddings = OpenAIEmbeddings(openai_api_key=key)
 # load vector database
-db = FAISS.load_local(
-    "Learnia_whatsapp/index", embeddings, allow_dangerous_deserialization=True
-)
-# define retriever
-retriever = db.as_retriever(
-    search_type="similarity_score_threshold",
-    search_kwargs={"k": 4, "score_threshold": 0.5},
-)
+db = FAISS.load_local("Learnia_whatsapp/index", embeddings, allow_dangerous_deserialization=True)
 # create a stuff documents chain.
 sd = StuffDocumentsChain(llm_chain=llm_chain)
 
 
 # obtain relevant documents from db
-def get_docs(query):
+def search(query, k, score_threshold):
+    # define retriever
+    retriever = db.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={"k": k, "score_threshold": score_threshold},
+    )
     docs = retriever.get_relevant_documents(query)
+    return docs
+
+
+def get_docs(query, k=4, score_threshold=0.5):
+    docs = search(query, k, score_threshold)
     return sd._get_inputs(docs)["context"]  # pylint: disable=protected-access
