cdd18ea6a66ac41047842df0d1cd4d3d4b4cbe29
diemtca <diemtca@gmail.com>
2024-03-19 11:05:35 -0500

    ajustes a la función/ debug

--- delete_blobs/__init__.py
+++ delete_blobs/__init__.py
@@ -1,7 +1,6 @@
 import azure.functions as func
-import asyncio
-
-from azure.storage.blob.aio import BlobServiceClient
+import logging
+from azure.storage.blob import BlobServiceClient
 from datetime import datetime, timedelta
 import pytz
 
@@ -10,26 +9,26 @@ import pytz
 ################################################################################################
 connect_str = "DefaultEndpointsProtocol=https;AccountName=historialconversaciones;AccountKey=1IyXT4sLXaaDhLf8Ljp6rxUO9juwpB1bBkdOiqC+9vyqeGTdMr7KpVc6PgZcQc+S4GWkMpkeRP+++AStiJs2AQ==;EndpointSuffix=core.windows.net"
 my_blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-my_container_name = "conversacionesdevconstructora"
+my_container_name = "conversacioneslernia"
 
-
-async def delete_blobs_after_24_hours(
+def delete_blobs_after_24_hours(
     blob_service_client: BlobServiceClient, container_name: str
 ):
-    container_client = blob_service_client.get_container_client(
-        container=container_name
-    )
-    async for blob in container_client.list_blobs():
+    container_client = blob_service_client.get_container_client(container_name)
+    for blob in container_client.list_blobs():
         blob_client = container_client.get_blob_client(blob.name)
         # Crear una zona horaria UTCZ
         utc = pytz.utc
         # Localizar el objeto datetime que no tiene zona horaria
         now = utc.localize(datetime.now())
         # Comparar con el objeto datetime que tiene zona horaria
-        if blob.creation_time + timedelta(hours=18) < now:
-            await blob_client.delete_blob()
-
+        try:
+             
+            if blob_client.exists() and blob.creation_time + timedelta(hours=23) < now:
+                blob_client.delete_blob()
+        except ResourceNotFoundError:
+                logging.info(f"El blob || {blob.name} || no fue encontrado.")
 
 def main(mytimer: func.TimerRequest) -> None:  # pylint: disable=unused-argument
     # Call the function to delete all blobs in the container that are older than 24 hours
-    asyncio.run(delete_blobs_after_24_hours(my_blob_service_client, my_container_name))
+    delete_blobs_after_24_hours(my_blob_service_client, my_container_name)
