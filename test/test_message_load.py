449395724b73fc359513a1ead6a27537fecaf44b
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2024-04-11 15:27:52 -0500

    delete unnecessaries

--- test/test_message_load.py
+++ test/test_message_load.py
@@ -1,12 +1,9 @@
 import pytest
-import json
 from simulation.user_messages import basic_number
 from Postgres.postgres import create_postgres_connection as get_connection
 from simulation.async_simulation import main as simulate_async_messages
 import asyncio
-from azure.core.exceptions import ResourceNotFoundError
-from azure.cosmos import CosmosClient, exceptions
-import os
+
 n_users = 5
 n_iterations = 5
 endpoint = "http://localhost:7071/api/Learnia_whatsapp"
@@ -14,26 +11,7 @@ endpoint = "http://localhost:7071/api/Learnia_whatsapp"
 expected_count = 2 * n_iterations + 1
 
 
-
-# Función de limpieza para ser llamada después de cada prueba
-
-
-# def limpiar_archivos():
-#     for i in range(n_users):
-#         archivo_usuario = f"{basic_number}{i}.txt"
-#         archivo_usuario_tmp = f"{basic_number}{i}$.txt"
-#         try:
-#             blob = container_client.get_blob_client(archivo_usuario)
-#             blob.delete_blob()
-#             blob_tmp = container_client.get_blob_client(archivo_usuario_tmp)
-#             blob_tmp.delete_blob()
-#         except ResourceNotFoundError as e:
-#             print(f"Error al limpiar el blob: {e}")
-
-
 # Fixture para simular mensajes antes de todas las pruebas y limpiar después
-
-
 @pytest.fixture(scope="session", autouse=True)
 def preparar_y_limpiar():
     # Ejecutar simulación antes de las pruebas
