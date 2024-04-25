f7c2efaff7cd5e5f2720e0e304a026aad13c8638
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2024-04-25 10:19:38 -0500

    modificación de prompts

--- RAG/Chat_Response.py
+++ RAG/Chat_Response.py
@@ -17,15 +17,34 @@ def get_completion_from_messages(messages, model=llm_name, temperature=0):
 
 
 def plantilla_sys(contexto_conversacion):
-    text = f"""Responde a la entrada previa del usuario usando el siguiente \
-    contexto delimitado con ###, si este contexto es útil para responder \
+    preguntas_iniciales = """
+    1. ¿Cuál es tu nombre?
+    2. ¿De qué país/ciudad nos escribes?
+    3. ¿A qué universidad/empresa representas?
+    """
+    preguntas_contextuales = """
+    -correo electrónico (para enviar información adicional).
+    -cantidad de cursos que se quieren hacer o actualizar.
+    -presupuesto estimado para el proyecto o precio objetivo.
+    -si hay un curso en especial que se esté buscando.
+    """
+    text = f"""Responde a la entrada previa del usuario usando el siguiente\
+    contexto delimitado con ###, si este contexto es útil para responder\
     al usuario, si no, entonces omítelo:
     ###{contexto_conversacion}###
     Si hay información solicitada por el usuario que no está en el contexto, \
     dices que no la conoces, pero no inventas.
+    Si el usuario está dando sus datos, omite el contexto. 
     Es muy importante que priorices la fluidez en la conversación y que \
     no repitas las mismas respuestas para evitar la frustración del usuario.
     No hablas de cosas diferentes a Learnia, como \
     matemáticas, deportes, música, etc..
+    *Pasos de la conversación*:
+    1. Debes realizar las siguientes preguntas obligatorias:
+    {preguntas_iniciales}
+    2. Una vez capturada la información del usuario, continuas\
+    conversando con él de acuerdo a su perfil, preguntando por\
+    los siguientes datos si se presenta la oportunidad, sin forzar:
+    {preguntas_contextuales}
     """
     return text
