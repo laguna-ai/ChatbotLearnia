92fc40e70d95f2c7f1e3129f836614d4a88ed33e
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2024-05-13 16:59:40 -0500

    creacion bd vectorial

--- knowledge_preparation/QandA.py
+++ knowledge_preparation/QandA.py
@@ -1,29 +1,71 @@
 import pandas as pd
 import json
+from RAG.Chat_Response import get_completion_from_messages
 
-# Load the CSV file
-file_path = 'Q&A.csv'
-data = pd.read_csv(file_path)
+def get_QandA_extended(file_path = 'Q&A.csv'):
+    # Load the CSV file
+    data = pd.read_csv(file_path)
 
-# Initialize a list to hold the JSON output
-json_output = []
+    # Initialize a list to hold the JSON output
+    QandA_extended = []
 
-# Iterate over columns starting from the second column
-for column in data.columns[1:]:
-    # Extract the question from the column header
-    question = column
+    # Iterate over columns starting from the second column
+    for column in data.columns[1:]:
+        # Extract the question from the column header
+        question = column
+        
+        # Extract the three answers from the column values
+        answers = data[column].dropna().tolist()
+        
+        # Append the question and answers to the JSON output list
+        QandA_extended.append({
+            "pregunta": question,
+            "respuestas": answers
+        })
+
+    n=len(QandA_extended)
+    print(f"Número de preguntas/respuestas encontradas: {n}")
+    return QandA_extended
+
+
+def get_synth_template(question_and_responses):
+    p=question_and_responses["pregunta"]
+    [r1,r2,r3]=question_and_responses["respuestas"]
+    template=f""" A continuación encontrarás una pregunta junto\
+    junto con tres respuestas.
+    ### 
+    pregunta: {p}
+    -respuesta1: {r1}
+    -respuesta2: {r2}
+    -respuesta3: {r3}
+    ###
+    Construye una única respuesta que resuma las tres respuestas.
+    La respuesta debe ser de un sólo párrafo y tener la información esencial\
+    y factual de las tres respuestas, sin salirse del contexto de éstas.
+    """
+    return template
+
+
+def get_synth_responses(QandA_extended):
+    faqs = []
+    for item in QandA_extended:
+        prompt = {"role": "system", "content": get_synth_template(item)}
+        response = get_completion_from_messages([prompt])[0]  # Assuming the response is in the first element
+        faqs.append({
+            "pregunta": item["pregunta"],
+            "respuesta": response
+        })
     
-    # Extract the three answers from the column values
-    answers = data[column].dropna().tolist()
-    
-    # Append the question and answers to the JSON output list
-    json_output.append({
-        "pregunta": question,
-        "respuestas": answers
-    })
+    # Convert the list of FAQs to a DataFrame
+    faqs_df = pd.DataFrame(faqs)
 
-n=len(json_output)
-print(f"Número de preguntas/repuestas: {n}")
+    # Save the DataFrame to a CSV file
+    output_file_path = 'FAQ.csv'
+    faqs_df.to_csv(output_file_path, index=False)
+    print(f"CSV file {output_file_path} has been created successfully.")
 
-for i in range(n):
-    print(json_output[i])   
+# Example usage
+
+QandA_extended = get_QandA_extended('Q&A.csv')
+get_synth_responses(QandA_extended)
+
---
+++ knowledge_preparation/vector_DB_creation.py
@@ -0,0 +1,52 @@
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings
+import os
+
+
+openai_api_key = os.environ["OPENAI_API_KEY"]
+
+
+def crear_base_conocimientos(config_archivos_csv, output_dir='.'):
+    """
+    Crea una base de conocimientos a partir de una lista de archivos CSV con configuraciones específicas.
+
+    :param config_archivos_csv: Lista de diccionarios con configuraciones de archivos CSV. 
+                                Cada diccionario debe tener las claves: 'file_path', 'encoding', y 'delimiter'.
+    :param output_dir: Directorio donde se guardará la base de datos vectorial. Por defecto es el directorio actual.
+    """
+    # Cargar y combinar todos los datos de los archivos CSV
+    datos_combinados = []
+    for config in config_archivos_csv:
+        loader = CSVLoader(file_path=config['file_path'], encoding=config['encoding'], csv_args={'delimiter': config['delimiter']})
+        datos = loader.load()
+        datos_combinados.extend(datos)
+
+    # Crear embeddings
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    vectorstore = FAISS.from_documents(datos_combinados, embeddings)
+
+    # Guardar la base de datos vectorial
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    vectorstore.save_local(output_dir)
+    print(f"Base de conocimientos creada y guardada en {output_dir}")
+
+
+
+# Ejemplo de uso
+
+config_archivos_csv = [
+     {'file_path': "csv_conocimientos/general.csv", 
+     'encoding': 'UTF-8', 
+     'delimiter': ';'},
+    {'file_path': "csv_conocimientos/cursos.csv", 
+     'encoding': 'UTF-8', 
+     'delimiter': ';'},
+    {'file_path': "csv_conocimientos/FAQ.csv", 
+     'encoding': 'UTF-8', 
+     'delimiter': ','}
+]
+output_dir = "index"  
+
+crear_base_conocimientos(config_archivos_csv, output_dir)
