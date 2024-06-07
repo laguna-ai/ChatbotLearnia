32de355915ebecafdb1a6fb0781ae3456443b951
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2024-06-07 16:30:47 -0500

    vectorstore creation

--- knowledge_preparation/index_creation.py
+++ knowledge_preparation/index_creation.py
@@ -1,19 +1,16 @@
 from langchain.document_loaders.csv_loader import CSVLoader
-from langchain_community.vectorstores.faiss import FAISS
-from langchain_openai import OpenAIEmbeddings
 import os
-
+from vectorstores.postgres import create_vectorstore
 
 openai_api_key = os.environ["OPENAI_API_KEY"]
 
 
-def create_index(config_archivos_csv, output_dir="."):
+def create_index(config_archivos_csv):
     """
-    Crea una base de conocimientos a partir de una lista de archivos CSV con configuraciones específicas.
+    Crea una base de conocimientos en postgres a partir de una lista de archivos CSV con configuraciones específicas.
 
     :param config_archivos_csv: Lista de diccionarios con configuraciones de archivos CSV.
                                 Cada diccionario debe tener las claves: 'file_path', 'encoding', y 'delimiter'.
-    :param output_dir: Directorio donde se guardará la base de datos vectorial. Por defecto es el directorio actual.
     """
     # Cargar y combinar todos los datos de los archivos CSV
     datos_combinados = []
@@ -26,17 +23,10 @@ def create_index(config_archivos_csv, output_dir="."):
         datos = loader.load()
         datos_combinados.extend(datos)
     # ver datos
-    print(datos_combinados)
+    print("DATOS:", datos_combinados)
 
     # Crear embeddings
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    vectorstore = FAISS.from_documents(datos_combinados, embeddings)
-
-    # Guardar la base de datos vectorial
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    vectorstore.save_local(output_dir)
-    print(f"Base de conocimientos creada y guardada en {output_dir}")
+    create_vectorstore(datos_combinados)
 
 
 # Ejemplo de uso
