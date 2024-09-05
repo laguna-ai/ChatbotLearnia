5835e73732805d7f5641c150ea524e77e8353531
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2024-09-05 18:00:34 -0500

    new functions!

--- MSAL/search.py
+++ MSAL/search.py
@@ -3,12 +3,18 @@ from .authentication import get_token
 import requests
 import pandas as pd
 import io
+import json
+
+graph_url="https://graph.microsoft.com/v1.0"
 
 def print_fields(response, fields):
     for user in response.json()["value"]:
         print(20*"***")
         for f in fields:
-            print(user[f])
+            if f in user.keys():
+                print(user[f])
+            else:
+                print(f"Warning: no field *{f}*")
 
 def get_response(url):
     token = get_token()
@@ -16,11 +22,20 @@ def get_response(url):
     response = requests.get(url, headers=headers)
     return response
 
+def post_request(url, body):
+    token = get_token()
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(body))
+    return response
+
 # Onedrive users
 
 def view_users():
     # Do a request for getting users
-    url = "https://graph.microsoft.com/v1.0/users"
+    url = f"{graph_url}/users"
     response = get_response(url)
     #print name and mail
     print_fields(response, ["displayName","mail"])
@@ -28,21 +43,21 @@ def view_users():
 # View my onedrive items
 
 def view_my_items(user_id):
-    url = f"https://graph.microsoft.com/v1.0/users/{user_id}/drive/root/children"
+    url = f"{graph_url}/{user_id}/drive/root/children"
     response = get_response(url)
     print_fields(response, ["name","id","webUrl"])
 
 # View items from a folder
 
 def view_folder_items(user_id, folder_id):
-    url = f"https://graph.microsoft.com/v1.0/users/{user_id}/drive/items/{folder_id}/children"
+    url = f"{graph_url}/users/{user_id}/drive/items/{folder_id}/children"
     response = get_response(url)
     print_fields(response, ["name","id","webUrl"])
 
 # Download a file by id
 
 def download_file(user_id, file_id):
-    url = f"https://graph.microsoft.com/v1.0/users/{user_id}/drive/items/{file_id}/content"
+    url = f"{graph_url}/users/{user_id}/drive/items/{file_id}/content"
     response = get_response(url)
     with open("MSAL/file.csv","wb") as file:
         file.write(response.content)
@@ -50,10 +65,53 @@ def download_file(user_id, file_id):
 # Get dataframe from file
 
 def get_df(user_id, file_id):
-    url = f"https://graph.microsoft.com/v1.0/users/{user_id}/drive/items/{file_id}/content"
+    url = f"{graph_url}/users/{user_id}/drive/items/{file_id}/content"
     response = get_response(url)
     df = pd.read_csv(io.BytesIO(response.content), sep=";")
     return df
+
+# Sharepoint sites
+
+def view_sites():
+    # Do a request for getting (Sharepoint) sites
+    url = f"{graph_url}/sites"
+    response = get_response(url)
+    print_fields(response, ["name","id","webUrl"])
+
+# View site items
+
+def view_site_items(site_id):
+    url = f"{graph_url}/sites/{site_id}/drive/root/children"
+    response = get_response(url)
+    print_fields(response, ["name","id","webUrl"])
+
+# View site lists
+
+def view_site_lists(site_id):
+    url = f"{graph_url}/sites/{site_id}/lists"
+    response = get_response(url)
+    print_fields(response, ["name","id","webUrl","description"])
+
+# Get list by id
+
+def get_list(site_id, list_id):
+    url = f"{graph_url}/sites/{site_id}/lists/{list_id}/items?expand=fields"
+    response = get_response(url)
+    for row in response.json()["value"]:
+        print(json.dumps(row["fields"], indent=4))
+    #print_fields(response, ["name","id","webUrl","description"])
+
+def add_to_list(site_id, list_id, data):
+    url = f"{graph_url}/sites/{site_id}/lists/{list_id}/items"
+    response = post_request(url, data)
+    if response.status_code == 201:
+        print("Item agregado exitosamente.")
+        #print(json.dumps(response.json(), indent=4))
+    else:
+        print(f"Error al agregar el item: {response.status_code}")
+        #print(response.text)
+    
+
 #view_users()
 
 user_ID = "2593866f-1608-46d9-a5b3-99d4efc86dec"
@@ -65,5 +123,30 @@ folder_ID = "01QJFTYXYDRWA2OZIFENCLR53LKU6WVML2" # Learnia folder
 file_ID = "01QJFTYXYIPGT5S5H6ZBG2UHLHPDUXW4QK"
 #download_file(user_ID, file_ID)
 
-DF = get_df(user_ID, file_ID)
-print(DF)
+#DF = get_df(user_ID, file_ID)
+#print(DF)
+
+#view_sites()
+
+site_ID = "lagunaai.sharepoint.com,9a2e4810-7465-473b-831b-62c1032b1015,4e47b86c-7705-4994-a0b3-861e3fcdcaa9"
+#view_site_items(site_ID)
+
+#view_site_lists(site_ID)
+
+list_ID = "fc2df33e-880f-41a7-b623-eff93a22c8bd" # lista de resúmenes de conversaciones de Learnia
+#get_list(site_ID,list_ID)
+
+info = {
+    "session_id": "dfMessenger-55793814-98c0-41c7-ba1c-ab8469be3f6c",
+    "nombre": "Mickey",
+    "procedencia": "Disney",
+    "email": "indeterminado",
+    "numero_de_cursos": "100",
+    "presupuesto": "US 1M",
+    "curso_de_interes": "indeterminado",
+    "cargo": "indeterminado",
+    "necesidad": "Disney courses",
+    "fecha": "2024-09-5T12:00:24Z",
+    "organizacion": "Disney World",
+}
+add_to_list(site_ID, list_ID, {"fields": info})
