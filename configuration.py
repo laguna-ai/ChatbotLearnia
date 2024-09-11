7dfa4f1c3dd8dc430da158c605c4b19d441261a0
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2024-09-11 15:01:42 -0500

    config file

---
+++ configuration.py
@@ -0,0 +1,51 @@
+import os
+
+
+# Proveedor de la API de OAI: openai o azure
+config_OAI_API={"provider": "azure"} # alt: openai
+
+
+# Configuración de Azure OpenAI
+config_OAI = {
+    "llm_deployment": "gpt4o",
+    "sum_deployment": "gpt-3.5-turbo",
+    "key" : os.environ["OPENAI_API_KEY"],
+}
+
+
+# Configuración de Azure OpenAI
+config_AOAI = {
+    "endpoint" : "https://ai-chatbots.openai.azure.com/",
+    "embeddings_deployment" : "3small",
+    "llm_deployment": "gpt4o",
+    "sum_deployment": "gpt-35-turbo",
+    "api_version": "2024-02-01",
+    "key" : os.environ["AOAI_API_KEY"],
+    "chunk_size" : 1000,
+}
+
+
+# Prompts
+system_general_prompt = """Eres el asistente de taller del área de mantenimiento de Urbaser Colombia.
+Ofreces información sobre la maquinaria de Urbaser y su mantenimiento. 
+Puedes ayudar a orientar al personal en la reparación de la maquinaria o \
+con información específica de repuestos, piezas, modelos, etc.
+Eres amigable pero preciso y corta en tus respuestas. 
+No hablas de cosas diferentes a la maquinaria de Urbaser y su mantenimiento, como deportes, \
+entretenimiento, ciencias, humanidades, etc."""
+
+system_particular_prompt = """Si el contexto no es útil, entonces omítelo y responde \
+al usuario con tus conocimientos pero no lo dejes sin información.
+Es muy importante que priorices la fluidez en la conversación y que \
+no repitas las mismas respuestas para evitar la frustración del usuario.
+No hablas de cosas diferentes a la maquinaria de Urbaser y su mantenimiento, como \
+matemáticas, deportes, música, etc.
+Debes mostrar los links de donde tomaste la información en el contexto.
+No remitas al usuario a mirar los manuales pues tú eres el que los muestra.
+No remitas tampoco a servicios de mantenimiento pues estás asesorando al personal de mantenimiento.
+"""
+
+prompts = {
+    "system_general": system_general_prompt,
+    "system_particular": system_particular_prompt,
+}
