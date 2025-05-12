41863c70d4b1a421c354c2e551ba284dc9cb51e2
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2025-05-12 11:50:23 -0500

    first version after migration

---
+++ MSAL/search.py
@@ -0,0 +1,46 @@
+# get a list of files in onedrive for Redimed folder
+from .authentication import get_token
+import requests
+import json
+import io
+import pandas as pd
+
+graph_url = "https://graph.microsoft.com/v1.0"
+site_ID = "lagunaai.sharepoint.com,9a2e4810-7465-473b-831b-62c1032b1015,4e47b86c-7705-4994-a0b3-861e3fcdcaa9"
+# lista de resúmenes de conversaciones de Learnia
+list_ID = "fc2df33e-880f-41a7-b623-eff93a22c8bd"
+
+
+def post_request(url, body):
+    token = get_token()
+    headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+    response = requests.post(url, headers=headers, data=json.dumps(body), timeout=10)
+    return response
+
+
+def add_to_list(data, site_id=site_ID, list_id=list_ID):
+    url = f"{graph_url}/sites/{site_id}/lists/{list_id}/items"
+    response = post_request(url, data)
+    if response.status_code == 201:
+        print("Item agregado exitosamente.")
+        # print(json.dumps(response.json(), indent=4))
+    else:
+        print(f"Error al agregar el item: {response.status_code}")
+        # print(response.text)
+
+
+def get_response(url):
+    token = get_token()
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.get(url, headers=headers, timeout=10)
+    return response
+
+
+# Get dataframe from excel file with known id
+
+
+def get_df(file_id, site_id=site_ID):
+    url = f"{graph_url}/sites/{site_id}/drive/items/{file_id}/content"
+    response = get_response(url)
+    df = pd.read_excel(io.BytesIO(response.content))
+    return df
---
+++ finish_sessions/blueprint.py
@@ -0,0 +1,38 @@
+# Register this blueprint by adding the following line of code 
+# to your entry point file.  
+# app.register_functions(blueprint) 
+# 
+# Please refer to https://aka.ms/azure-functions-python-blueprints
+
+import azure.functions as func
+import logging
+from Postgres.postgres import (
+    create_postgres_connection,
+)
+from Postgres.session_ending import (
+    get_sessions_to_finish,
+    finish_session,
+)
+from .insights import get_insights
+from MSAL.search import add_to_list
+
+blueprint = func.Blueprint()
+
+
+@blueprint.timer_trigger(schedule="0 0 5 * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def finish_sessions(myTimer: func.TimerRequest) -> None: # pylint: disable=unused-argument
+    
+    logging.info("Python timer trigger function started")
+    with create_postgres_connection() as conn:  # pylint: disable=E1129
+        logging.info("Connected to Postgres")
+        # Get the sessions that need to be finished 
+        sessions_to_finish = get_sessions_to_finish(conn)
+        logging.info(f"Sessions to finish: {len(sessions_to_finish)}")
+        for s in sessions_to_finish:
+            analysis = get_insights(s)
+            logging.info(f"Analysis of session {s[0]}: {analysis}")
+            add_to_list(analysis)
+            logging.info(f"Analysis Added to Sharepoint")
+            finish_session(conn, s)
+            logging.info(f"Session {s[0]} finished")
